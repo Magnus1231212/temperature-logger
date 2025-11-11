d64c8a510cdc8132d643ca7ae83424f2ff5d4f4f
asbjosh <asbjornsh1@gmail.com>
2025-11-11 12:49:47 +0100

    Lysdioder virker

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Diagnostics;
 using System.Threading;
+using temperature_logger.Modules;
+using static temperature_logger.Modules.Display;
 
 namespace temperature_logger
 {
@@ -8,8 +10,11 @@ namespace temperature_logger
     {
         public static void Main()
         {
-
             Display.DisplayOled();
+            Lysdioder.Setup(0.5);
+            UpdateSystem();
+
+
 
             Debug.WriteLine("Hello from nanoFramework!");
 
@@ -19,6 +24,15 @@ namespace temperature_logger
             // Browse our samples repository: https://github.com/nanoframework/samples
             // Check our documentation online: https://docs.nanoframework.net/
             // Join our lively Discord community: https://discord.gg/gCyBu8T
+        }
+
+        private static void UpdateSystem()
+        {
+            // Opdater displayet
+            Display.ShowTemperatureDisplay();
+
+            // Opdater lysdioder ud fra temperaturforskellen
+            Lysdioder.UpdateStatus(Display.CurrentTemperature, Display.DesiredTemperature);
         }
 
     }
